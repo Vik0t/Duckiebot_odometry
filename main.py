50cbed4a9583bf779037f66379009281e08d2417
Vik0t <89171514+Vik0t@users.noreply.github.com>
2022-05-28 17:53:47 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,32 +2,32 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def create_odometry_map(file):
+class Odometry:
     """
         Creates a map based on file input.
-        :param file: file that function reads
-        :return: map
     """
-    encoderL = []
-    encoderR = []
-    time = []
 
-    global robotLength
-    robotLength = 0.1
+    def __init__(self):
+        self.file = "Test.txt"  # add your path there
+        self.robotLength = 0.1
 
-    with open(file) as f:
-        content = f.readlines()
-
-    for line in content:
-        lineCut = line.split(" ")
-        encoderL.append(float(lineCut[0]))
-        encoderR.append(float(lineCut[1]))
-        time.append(float(lineCut[2]) / 10 ** 9)
+    def getInfo(self):
+        encoderL = []
+        encoderR = []
+        time = []
+        with open(self.file) as f:
+            content = f.readlines()
+        for line in content:
+            lineCut = line.split(" ")
+            encoderL.append(float(lineCut[0]))
+            encoderR.append(float(lineCut[1]))
+            time.append(float(lineCut[2]) / 10 ** 9)
         time_diff = np.diff(time)
+        return encoderL, encoderR, time_diff
 
-    def odometry(x, y, date, theta, left, right):
+    def odometry(self, x, y, date, theta, left, right):
         d = (left + right) / 2
-        fi = (right - left) / (2 * robotLength)
+        fi = (right - left) / (2 * self.robotLength)
         x1 = x + (date * d * np.cos(theta))
         y1 = y + (date * d * np.sin(theta))
 
@@ -35,13 +35,14 @@ def create_odometry_map(file):
 
         return x1, y1, theta1
 
-    def createMap():
+    def createMap(self):
         x = [0]
         y = [0]
         theta = [0]
+        encoderL, encoderR, time_diff = self.getInfo()
 
         for i in range(len(encoderL) - 1):
-            x1, y1, theta1 = odometry(x[i], y[i], time_diff[i], theta[i], encoderL[i], encoderR[i])
+            x1, y1, theta1 = self.odometry(x[i], y[i], time_diff[i], theta[i], encoderL[i], encoderR[i])
             x.append(x1)
             y.append(y1)
             theta.append(theta1)
@@ -49,6 +50,5 @@ def create_odometry_map(file):
         plt.plot(x, y)
         plt.show()
 
-    createMap()
-
-create_odometry_map('Test.txt')
+output = Odometry()
+output.createMap()
